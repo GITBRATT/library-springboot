5db8341f0ea2d3219770d70f548d17696b98df0f
Andrey Kurgaev <kurgaev.andrey@mail.ru>
2026-06-21 14:20:47 +0300

    fix: ExceptionHandler

--- src/main/java/com/spring_boot_pet/controller/GlobalExceptionHandler.java
+++ src/main/java/com/spring_boot_pet/controller/GlobalExceptionHandler.java
@@ -1,13 +1,24 @@
 package com.spring_boot_pet.controller;
 
+import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler implements ErrorController {
+
+    @RequestMapping("/error")
+    public String error() {
+        return "error-page";
+    }
+
     @ExceptionHandler
-    public void exceptionHandler(Exception ex, Model model) {
+    public String exceptionHandler(Exception ex, Model model) {
         model.addAttribute("msg", ex.getMessage());
+        return "redirect:/error-page";
     }
 }
